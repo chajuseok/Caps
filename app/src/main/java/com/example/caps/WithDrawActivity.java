6a80cb272aa7b2d46da747911a5698ef297a7c86
helloworldaaad <ehrhwn48@gmail.com>
2022-05-28 13:17:45 +0900

    출금이체 일부분

--- app/src/main/java/com/example/caps/WithDrawActivity.java
+++ app/src/main/java/com/example/caps/WithDrawActivity.java
@@ -15,6 +15,7 @@ import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.MenuItem;
 import android.view.View;
@@ -33,17 +34,26 @@ import com.google.android.gms.vision.text.TextRecognizer;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class WithDrawActivity extends AppCompatActivity {
 
     Spinner spinner;
     String bank_number; // 계좌번호
     String bank_id;
+    String money;
     ImageButton ocr;
     Button withdraw;
     Bitmap bitmap;
     EditText bank;
+    EditText moneye;
+    String fintech_use_num;
+    String access_token;
     private static final int REQUEST_CAMERA_CODE = 100;
 
     @Override
@@ -52,8 +62,12 @@ public class WithDrawActivity extends AppCompatActivity {
         setContentView(R.layout.activity_with_draw);
         ocr = findViewById(R.id.ocr);
         withdraw = findViewById(R.id.withdraw);
-
+        Intent intent = getIntent();
+        fintech_use_num = intent.getStringExtra("fintech_use_num");
+        access_token = intent.getStringExtra("access_token");
         bank = findViewById(R.id.bank_number);
+        moneye= findViewById(R.id.money);
+
         setSupportActionBar(findViewById(R.id.toolbar));
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 뒤로가기 버튼, 디폴트로 true만 해도 백버튼이 생김
@@ -97,13 +111,17 @@ public class WithDrawActivity extends AppCompatActivity {
         withdraw.setOnClickListener(new View.OnClickListener() {  // 송금이벤트처리
             @Override
             public void onClick(View view) {
-
+                money = moneye.getText().toString();
+                Withdraw th = new Withdraw();
+                th.start();
+                try {
+                    th.join();
+                }catch(InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
             }
         });
-
-
-
-
 
     }
 
@@ -155,6 +173,72 @@ public class WithDrawActivity extends AppCompatActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void Withdraw()
+    {
+        String address = "https://testapi.openbanking.or.kr/v2.0/transfer/withdraw/fin_num";
+        String PostBody = "{\r\n"
+                + "    \"bank_tran_id\": \""+GenTranId()+"\",\r\n"
+                + "    \"cntr_account_type\": \"N\",\r\n"
+                + "    \"cntr_account_num\": \"3021011435911\",\r\n"
+                + "    \"dps_print_content\": \"테스트\",\r\n"
+                + "    \"fintech_use_num\": \""+fintech_use_num+"\",\r\n"
+                + "    \"tran_amt\": \""+money+"\",\r\n"
+                + "    \"tran_dtime\": \"20220401113210\",\r\n"
+                + "    \"req_client_name\": \"김주열\",\r\n"
+                + "    \"req_client_num\": \"1101005239\",\r\n"
+                + "    \"transfer_purpose\": \"TR\",\r\n"
+                + "    \"req_client_fintech_use_num\": \"120220055688941039534617\",\r\n"
+                + "    \"recv_client_name\": \"차주석\",\r\n"
+                + "    \"recv_client_bank_code\": \"007\",\r\n"
+                + "    \"recv_client_account_num\": \"4564564564\"\r\n"
+                + "}";
+        try
+        {
+            URL url = new URL(address);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json; UTF-8");
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Authorization","BearereyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiIxMTAxMDA1MjM5Iiwic2NvcGUiOlsiaW5xdWlyeSIsImxvZ2luIiwidHJhbnNmZXIiXSwiaXNzIjoiaHR0cHM6Ly93d3cub3BlbmJhbmtpbmcub3Iua3IiLCJleHAiOjE2NjE0NzU1NDIsImp0aSI6ImQ4NmE2NTAwLTNlNmQtNDVkZC1iNjA3LWE4YTZmMTM4NGM2ZCJ9.SPCFlv3RCxgv7gryV5AwHLiRZ4xVpxBG6Ee1eJ9-8Vw");
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            try(OutputStream os = connection.getOutputStream())
+            {
+                byte request_data[] = PostBody.getBytes("UTF-8");
+                os.write(request_data, 0, request_data.length);
+            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            //응답 내용
+            String line = null;
+            while((line = in.readLine()) != null)
+            {
+                System.out.println(line);
+            }
+            in.close();
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+    public String GenTranId()
+    {
+        String tranId = "M202200556U";
+        for(int i =0; i<9; i++)
+        {
+            int num = (int)(Math.random() * 10);
+            tranId += Integer.toString(num);
+        }
+        return tranId;
+    }
+    public class Withdraw extends Thread
+    {
+        public void run()
+        {
+            Withdraw();
+        }
     }
 
 
